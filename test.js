0819b34ac6b659c4a5df4e43f76170529aea187a
Jade Jellicoe <jellicoe_currivan_dani@network.elanco.com>
2021-03-03 01:15:35 +0000

    more restful stuff

--- test.js
+++ test.js
@@ -8,6 +8,23 @@ app.get('/',(req,res)=>{
   res.send('Hello Virtual Machine!')
 })
 
+app.put('/', (req, res) => {
+    res.send('put request')
+})
+
+app.delete('/', (req, res) => {
+    res.send('delete request')
+})
+
+app.post('/', (req, res) => {
+    res.send('post request')
+})
+
+app.get('/service', (req, res) => {
+    res.send('service retrieved')
+})
+
+
 app.listen(port,()=>{
   console.log(`Express Application  listening at port 3000`)
 })
